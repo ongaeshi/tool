314fc0701d5316f8a0ce2acf79cf925930a1b1ca
ongaeshi <ongaeshi0621@gmail.com>
2020-05-10 00:18:30 +0900

    feat: Import Caseninja

--- main.rb
+++ main.rb
@@ -1,5 +1,66 @@
+# https://github.com/ongaeshi/caseninja
+
+module Caseninja
+  VERSION = "0.0.1"
+end
+
+module Caseninja
+  module_function
+
+  def to_hash(text)
+    {
+      space: to_space(text),
+      chain: to_chain(text),
+      snake: to_snake(text),
+      camel: to_camel(text),
+      pascal: to_pascal(text),
+      upchain: to_upchain(text),
+      upsnake: to_upsnake(text),
+    }
+  end
+
+  def to_space(text)
+    normalize(text)
+  end
+
+  def to_chain(text)
+    normalize(text).gsub(" ", "-")
+  end
+
+  def to_snake(text)
+    normalize(text).gsub(" ", "_")
+  end
+
+  def to_camel(text)
+    to_pascal(text).sub(/./) { $&.downcase }
+  end
+
+  def to_pascal(text)
+    normalize(text).split.map { |e| e.capitalize }.join
+  end
+
+  def to_upchain(text)
+    to_chain(text).upcase
+  end
+  
+  def to_upsnake(text)
+    to_snake(text).upcase
+  end
+
+  def normalize(text)
+    if text =~ / /
+      text.downcase
+    elsif text =~ /-/
+      text.gsub("-", " ").downcase
+    elsif text =~ /_/
+      text.gsub("_", " ").downcase
+    else
+      text.split(/(?=[A-Z])/).join(" ").downcase
+    end
+  end
+end
+
+
 def main(src)
-  # eval(src)
-  src.gsub('foo', 'bar')
-  # src.scan(/foo/).join("\n")
+  Caseninja.to_hash(src).values.join("\n")
 end
